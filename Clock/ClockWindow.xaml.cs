dc3a405651d21cf7bec9a124fcdb36138a4b4222
fenggwsx <fenggwsx@foxmail.com>
2021-04-30 17:05:45 +0800

    添加项目文件。

---
+++ Clock/ClockWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Clock
+{
+    public partial class ClockWindow : Window
+    {
+        private int startTick;
+        private int curTick;
+        private int totTick;
+        private readonly DispatcherTimer disTimer = new DispatcherTimer();
+        private readonly SolidColorBrush colorNormal = new SolidColorBrush(Colors.Green);
+        private readonly SolidColorBrush colorWarning = new SolidColorBrush(Colors.Red);
+        private readonly ConsoleWindow consoleWindow = new ConsoleWindow();
+        public ClockWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void StartClock(double seconds)
+        {
+            totTick = (int)(seconds * 1000);
+            startTick = Environment.TickCount;
+            disTimer.Tick += new EventHandler(DisTimer_Tick);
+            disTimer.Interval = new TimeSpan(1); // 0为一直执行 占用CPU过高
+            disTimer.Start();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // 相应消息以支持鼠标在任意地方拖动窗口
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+
+        public void DisTimer_Tick(object sender, EventArgs e)
+        {
+            curTick = Environment.TickCount;
+            if (curTick > startTick + totTick)
+            {
+                ProgressBar.Value = 100; // 计时结束设为100%
+                Tip.Text = "时间到"; // 计时结束提示文本
+                disTimer.Stop(); // 计时停止
+            }
+            else
+            {
+                double percentage = (curTick - startTick) * 100 / (double)totTick; // 计算百分比
+                if (percentage > 90) ProgressBar.Foreground = colorWarning; // 颜色改为警告的红色
+                else ProgressBar.Foreground = colorNormal; // 颜色为普通的绿色
+                ProgressBar.Value = percentage; // 设置进度
+                Tip.Text = 
+                    string.Format("{0:D2}", (totTick - curTick + startTick) / 1000 / 60) + ':' + 
+                    string.Format("{0:D2}",(totTick - curTick + startTick) / 1000 % 60); // 格式化输出
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            consoleWindow.Owner = this;
+            consoleWindow.WindowCloseEvent += Close;
+            consoleWindow.StartClockEvent += StartClock;
+            consoleWindow.TopmostWindowEvent += delegate (bool top) { Topmost = top; };
+            consoleWindow.MaximizeClockWindowEvent += delegate (bool maximize) { WindowState = maximize ? WindowState.Maximized : WindowState.Normal; };
+            consoleWindow.ShowClockWindowEvent += delegate (bool show) { Visibility = show ? Visibility.Visible : Visibility.Hidden; };
+            consoleWindow.TopmostWindow.IsChecked = true;
+            consoleWindow.ShowClockWindow.IsChecked = true;
+            consoleWindow.Show();
+        }
+    }
+}
---
+++ Clock/ConsoleWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Clock
+{
+    public delegate void WindowCloseDelegate();
+    public delegate void StartClockDelegate(double seconds);
+    public delegate void TopmostWindowDelegate(bool top);
+    public delegate void MaximizeClockWindowDelegate(bool maximize);
+    public delegate void ShowClockWindowDelegate(bool show);
+    public partial class ConsoleWindow : Window
+    {
+
+        public event WindowCloseDelegate WindowCloseEvent;
+        public event StartClockDelegate StartClockEvent;
+        public event TopmostWindowDelegate TopmostWindowEvent;
+        public event MaximizeClockWindowDelegate MaximizeClockWindowEvent;
+        public event ShowClockWindowDelegate ShowClockWindowEvent;
+        public ConsoleWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_StartClock(object sender, RoutedEventArgs e)
+        {
+            if (TimeInput.Text != null && System.Text.RegularExpressions.Regex.IsMatch(TimeInput.Text, @"^([1-9]\d*(\.\d+)?|0\.\d*[1-9]\d*)$")) // 正则表达式验证输入合法性
+            {
+                double time = double.Parse(TimeInput.Text); // 解析
+                StartClockEvent(time * 60);
+            }
+            else
+            {
+                MessageBox.Show("请输入一个正确的时长", "输入错误", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            WindowCloseEvent();
+        }
+
+        private void TopmostWindow_Checked(object sender, RoutedEventArgs e)
+        {
+            Topmost =  true;
+            TopmostWindowEvent(true);
+        }
+
+        private void TopmostWindow_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Topmost = false;
+            TopmostWindowEvent(false);
+        }
+
+        private void MaximizeClockWindow_Checked(object sender, RoutedEventArgs e)
+        {
+            MaximizeClockWindowEvent(true);
+        }
+
+        private void MaximizeClockWindow_Unchecked(object sender, RoutedEventArgs e)
+        {
+            MaximizeClockWindowEvent(false);
+        }
+
+        private void ShowClockWindow_Checked(object sender, RoutedEventArgs e)
+        {
+            ShowClockWindowEvent(true);
+        }
+
+        private void ShowClockWindow_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ShowClockWindowEvent(false);
+        }
+    }
+}
